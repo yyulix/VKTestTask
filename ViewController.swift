da61913a5eeeb7e1b98692a16184b2becdd6263e
Yulia Popova <popova.yua@edu.spbstu.ru>
2022-03-25 22:16:49 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VKTestTask
+//
+//  Created by Yulia Popova on 25/3/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
