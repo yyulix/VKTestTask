7dc28b864c90a4cbff784f9426d253a8a9f43d04
Yulia Popova <popova.yua@edu.spbstu.ru>
2022-03-26 14:03:06 +0300

    Add Data Manager

---
+++ DataManager.swift
@@ -0,0 +1,24 @@
+//
+//  DataManager.swift
+//  VKTestTask
+//
+//  Created by Yulia Popova on 26/3/2022.
+//
+
+import Foundation
+
+struct DataManager {
+    static let shared = DataManager()
+
+    func getData(completion: @escaping ([MeasurementModel]) -> Void) {
+            
+        var data : [MeasurementModel] = []
+        
+        for _ in 1...100 {
+            let measure = MeasurementModel(measurement: Double.random(in: 0..<1000), time: Date())
+            data.append(measure)
+        }
+
+        completion(data)
+    }
+}
