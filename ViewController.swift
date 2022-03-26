73b5a594030e338376f47caba2e415dc56240ac4
yyulix <yuliarandomchenko@gmail.com>
2022-03-26 16:37:57 +0300

    Add deselection on click

--- ViewController.swift
+++ ViewController.swift
@@ -158,6 +158,10 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         guard let section = TableSection(rawValue: indexPath.section) else { return }
         guard !data.isEmpty else { return }
